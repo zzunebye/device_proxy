2d5bc951da7b3db44faa73c08f7ba9837aff7e40
June <bhang96@gmail.com>
2022-03-28 12:10:45 +0800

    WIP: Android Embedding

--- android/src/main/kotlin/com/intechlab/device_proxy/DeviceProxyPlugin.kt
+++ android/src/main/kotlin/com/intechlab/device_proxy/DeviceProxyPlugin.kt
@@ -6,11 +6,15 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+
 import android.app.Activity
 
 
 
-class DeviceProxyPlugin(val activity: Activity?) : MethodCallHandler {
+class DeviceProxyPlugin(val activity: Activity?) : FlutterActivity {
 
   companion object {
     @JvmStatic
